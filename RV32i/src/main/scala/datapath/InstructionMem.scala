2f3cc3a509f1a8bb7978f5ee0d545453ed3603f3
Hadir Khan <hadirkhan@hotmail.com>
2020-02-16 17:36:48 +0500

    Changed the path of instruction file from ubuntu path to mac path

--- RV32i/src/main/scala/datapath/InstructionMem.scala
+++ RV32i/src/main/scala/datapath/InstructionMem.scala
@@ -10,7 +10,7 @@ class InstructionMem extends Module {
     })
 
     val mem = Mem(1024, UInt(32.W))
-    loadMemoryFromFile(mem, "/home/hadirkhan/Desktop/mem1.txt")
+    loadMemoryFromFile(mem, "/Users/mbp/Desktop/mem1.txt")
     io.readData := mem(io.wrAddr)
 
     //io.readData := "h00200093".U
