555cf31524fbacfb18afb685377d8fed17d254a8
Hadir Khan <hadirkhan10@gmail.com>
2020-03-25 02:44:40 +0500

    Created the functionality of loading instructions from a text file

--- RV32i/src/test/scala/core/SocTests.scala
+++ RV32i/src/test/scala/core/SocTests.scala
@@ -1,4 +1,5 @@
 package core
+import scala.io.Source
 import chisel3.iotesters.PeekPokeTester
 import soc.Soc
 
@@ -7,97 +8,16 @@ class SocTests(c: Soc) extends PeekPokeTester (c){
 //  poke(c.io.gpio_2, 0)
 //  poke(c.io.gpio_3, 0)
 //  poke(c.io.gpio_4, 1)
-  val insts = Array(0xf9010113,
-0x06812623,
-0x07010413,
-0xfa042e23,
-0xfc042023,
-0xfc042223,
-0xfc042423,
-0xfc042623,
-0xfc042823,
-0xfc042a23,
-0xfc042c23,
-0xfc042e23,
-0xfe042023,
-0xfe042223,
-0x00b00793,
-0xfaf42e23,
-0x01d00793,
-0xfcf42023,
-0x05300793,
-0xfcf42223,
-0x06d00793,
-0xfcf42423,
-0x05a00793,
-0xfcf42623,
-0x07000793,
-0xfcf42823,
-0x06200793,
-0xfcf42a23,
-0x04300793,
-0xfcf42c23,
-0x04d00793,
-0xfcf42e23,
-0x02d00793,
-0xfef42023,
-0xf8042823,
-0xf8042a23,
-0xf8042c23,
-0xf8042e23,
-0xfa042023,
-0xfa042223,
-0xfa042423,
-0xfa042623,
-0xfa042823,
-0xfa042a23,
-0xfa042c23,
-0x05900793,
-0xf8f42823,
-0x03e00793,
-0xf8f42a23,
-0x04900793,
-0xf8f42c23,
-0x05a00793,
-0xf8f42e23,
-0x00700793,
-0xfaf42023,
-0x05900793,
-0xfaf42223,
-0x02200793,
-0xfaf42423,
-0x01400793,
-0xfaf42623,
-0x04800793,
-0xfaf42823,
-0x06400793,
-0xfaf42a23,
-0xfe042423,
-0xfe042623,
-0x0400006f,
-0xfec42783,
-0x00279793,
-0xff040713,
-0x00f707b3,
-0xfcc7a703,
-0xfec42783,
-0x00279793,
-0xff040693,
-0x00f687b3,
-0xfa07a783,
-0x00f707b3,
-0xfef42423,
-0xfec42783,
-0x00178793,
-0xfef42623,
-0xfec42703,
-0x00900793,
-0xfae7dee3,
-0x00000013,
-0x00000013,
-0x06c12403,
-0x07010113, 0x00000fff)
-//val insts = Array(0x00004237, 0x00000fff)
+val filePath = "/Users/mbp/Desktop/mem1.txt"
+val bufferedSource =  Source.fromFile(filePath)
+val fileData = bufferedSource.getLines.toArray
+// We create an array insts by reading the lines from the text file and parsing the string
+// as hexadecimal. The parse function does not accept 0x literals so we used substring(2)
+// to get rid of the first two characters which are 0x finally "yielding" it into an array.
+val insts = for (i <- fileData) yield java.lang.Long.parseLong(i.substring(2), 16)
+// closing the opened file
+bufferedSource.close
+
   poke(c.io.rxd, 1)
   //step(10)
   for (inst <- insts) {
