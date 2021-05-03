4b25677c9a44c471abf4c2c5fd6916d33210d72a
Hadir Khan <hadirkhan10@gmail.com>
2021-05-03 17:39:47 +0500

    created a testbench with optional memory dump file argument

--- RV32i/src/test/scala/HarnessTester.scala
+++ RV32i/src/test/scala/HarnessTester.scala
@@ -1,6 +1,24 @@
+import caravan.bus.wishbone.WishboneConfig
 import chisel3._
-import chisel3.iotesters.PeekPokeTester
-
-class HarnessTester(c: Harness) extends PeekPokeTester(c) {
-  step(100)
+import org.scalatest._
+import chiseltest._
+import chiseltest.ChiselScalatestTester
+import chiseltest.internal.VerilatorBackendAnnotation
+import chiseltest.experimental.TestOptionBuilder._
+import org.scalatest.FreeSpec
+class HarnessTester extends FreeSpec with ChiselScalatestTester {
+  def getFile: Option[String] = {
+    if (scalaTestContext.value.get.configMap.contains("memFile")) {
+      Some(scalaTestContext.value.get.configMap("memFile").toString)
+    } else {
+      None
+    }
+  }
+  "it should work" in {
+    implicit val config = WishboneConfig(10, 32)
+    val programFile = getFile
+    test(new Harness(programFile)).withAnnotations(Seq(VerilatorBackendAnnotation)) {c =>
+      c.clock.step(100)
+    }
+  }
 }
